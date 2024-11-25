dbc05e28f0d595c33e2f8f83e84f0aadc34ec3b0
Cédric Truyen <cedrictruyen@icloud.com>
2024-11-25 16:39:43 +0100

    Nouveau seeding sur les users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Database\Seeders\PostSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PronosticSeeder::class,
+            PostSeeder::class,
         ]);
     }
 }
